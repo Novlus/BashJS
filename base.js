fd43bfc3c593aa942f6c62efabcb2eee06b344dc
AlainMinhNguyen <alain.minh.nguyen@gmail.com>
2022-10-17 01:01:34 +0200

    Ajout nb d'essais max + Nb max de caractères dans le nom du joueur

--- base.js
+++ base.js
@@ -4,18 +4,30 @@ console.log(nb)
 let string
 let number
 
+let max_attempt = 10
+let attempt = 0
+let max_name_length = 10
+let max_score_saved = 10
+
 while(number != nb)
 {
+    if (attempt == max_attempt)
+    {
+        console.log("Vous avez perdu, le nombre était: " + nb)
+        break
+    }
     string = prompt("Entrez un nombre entre 1 et 99")
     number = parseInt(string)
     if( number < nb)
     {
         console.log("le nombre que vous cherchez est plus grand")
+        attempt++
     }
 
     else if( number > nb)
     {
         console.log("le nombre que vous cherchez est plus petit")
+        attempt++
     }
 
     else 
@@ -35,3 +47,11 @@ while(isNumber == 0)
 }*/
 }
 
+string = prompt("Entrez votre nom")
+player_name = parseInt(string)
+
+while (player_name.length > max_name_length)
+{
+    string = prompt("Votre nom est trop long, veuillez en choisir un plus court")
+    player_name = parseInt(string)
+}
