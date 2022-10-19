fee579fd0bc1a3940bb044fbd0fddd0ea611fdd2
lgyn57 <axel.leguyen@gmail.com>
2022-10-19 22:46:14 +0200

    Nom, score, position, file

--- base.js
+++ base.js
@@ -3,6 +3,36 @@ let nb = Math.floor(Math.random() * 99) + 1;
 console.log(nb)
 let string
 let number
+let score = 0;
+
+// nom du joueur
+function getname() {
+    let username = prompt("Quel est votre nom ?");
+    return username;
+}
+
+// créer et enregistrer le score dans un fichier
+function createFile() {
+    let fs = require('fs')
+    fs.writeFile('score.txt', 'score', function (err) {
+        if (err) throw err;
+        console.log('Fichier créé !');
+    });
+    fs.writeline(score);
+    fs.writeline(" --- ");
+    fs.writeline(username);
+    fs.writeline('\n');
+}
+
+// afficher la position du joueur
+function positonScore() {
+    let fs = require('fs')
+    fs.readFile('score.txt', 'utf8', function (err, data) {
+        if (err) throw err;
+        const tri = data.sort();
+        console.log(tri);
+    });
+}
 
 while(number != nb)
 {
@@ -11,27 +41,23 @@ while(number != nb)
     if( number < nb)
     {
         console.log("le nombre que vous cherchez est plus grand")
+        score ++;
     }
 
     else if( number > nb)
     {
         console.log("le nombre que vous cherchez est plus petit")
+        score ++;
     }
 
     else 
     {
         console.log("Bravo vous avez gagné")
+        score ++;
+        console.log("Votre score est de " + score)
+        getname();
+        createFile();
+        positonScore();
     }
-//let isNumber = 0
-/*if (NaN(number) == true) { // C'est bien un nombre
-    isNumber =0;}
-else{
-    isNumber =1;
-}
-while(isNumber == 0)
-{
-    string = prompt("Entrez un nombre entre 1 et 99")
-    number = parseInt(string)
-}*/
 }
 
