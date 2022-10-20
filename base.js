2e8c5c59ec8f85994c938fa2f2a53b8542f55678
AlainMinhNguyen <alain.minh.nguyen@gmail.com>
2022-10-20 08:43:36 +0200

    file config

--- base.js
+++ base.js
@@ -4,10 +4,13 @@ console.log(nb)
 let string
 let number
 
-let max_attempt = 10
-let attempt = 0
-let max_name_length = 10
-let max_score_saved = 10
+
+import { config } from './config.js';
+
+let attempt = config.attempt
+let max_attempt = config.max_attempt
+let max_name_length = config.max_name_length
+let max_score_saved = config.max_score_saved
 
 while(number != nb)
 {
@@ -34,17 +37,7 @@ while(number != nb)
     {
         console.log("Bravo vous avez gagné")
     }
-//let isNumber = 0
-/*if (NaN(number) == true) { // C'est bien un nombre
-    isNumber =0;}
-else{
-    isNumber =1;
-}
-while(isNumber == 0)
-{
-    string = prompt("Entrez un nombre entre 1 et 99")
-    number = parseInt(string)
-}*/
+
 }
 
 let player_name = prompt("Entrez votre nom")
---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    max_attempt : 10,
+    attempt : 0,
+    max_name_length : 10,
+    max_score_saved : 10
+};
+export { config };
