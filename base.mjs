57204836448549fe3d44804cb14f83313bae17a1
Damien <damien@LAPTOP-OE4I5EUT.localdomain>
2022-11-07 19:56:35 +0100

    tableau des scores

--- base.mjs
+++ base.mjs
@@ -12,8 +12,19 @@ function setDifficulty(level) {
     }
 }
 
+function ouiNon(param) {
+    if (param == "o") {
+        return true;
+    }
+    else if (param == "n") {
+        return false;
+    }
+}
+
+let score = [] ;
+
 function addScore(score, name, nbTry) {
-    score.push(name, nbTry)
+    score.push(String(name), String(nbTry))
 }
 
 // créer et enregistrer le score dans un fichier
@@ -40,27 +51,98 @@ function positonScore() {
 }
 
 function getScore(score) {
-    let name = prompt("Entrez le nom du joueur dont vous voulez voir le score : ")
-    if (score.indexOf(name) != -1) {
-        console.log(score[score.indexOf(name)], score[score.indexOf(name) + 1])
-    }
-    else {
-        console.log("Le joueur n'existe pas")
-    }
+    document.getElementById("score").style.display = "none";
+    document.getElementById("joueurScore").style.display = "block";
+    document.getElementById("valider-score2").addEventListener("click", function () {
+        let name ="";
+        name = document.getElementById("score-reponse2").value;
+        String(name);
+        console.log(name)
+        console.log(score.indexOf(name))
+
+        document.getElementById("scoreJoueur").style.display = "block";
+        if (score.indexOf(name) != -1) {
+            document.getElementById("scoreJoueur").innerHTML += "<p>"+score[score.indexOf(name)] +" "+score[score.indexOf(name) + 1]+"</p>"
+            console.log(score[score.indexOf(name) + 1])
+            document.getElementById("joueurScore").style.display = "none";
+        }
+        else {
+            document.getElementById("scoreJoueur").innerHTML += "Le joueur n'existe pas"
+            document.getElementById("joueurScore").style.display = "none";
+        }
+        document.getElementById("tousScore").style.display = "block";
+        document.getElementById("tousScore").innerHTML = "<p>Voulez vous voir le tableau des scores ? (O/N) : </p>  <input type='text' id='choixTousScore' name ='choixTousScore' placeholder='(O/N)'> <input type='button' id='valider-tousScore' value='Valider'>";
+        document.getElementById("valider-tousScore").addEventListener("click", function () {
+        let answer2 = document.getElementById("choixTousScore").value
+        if (answer2 == "o" || answer2 == "O")
+        {
+            getAllScore(score)
+        }})
+    })
+    
 }
 
 
-function getAllScore(score)
-{
+function getAllScore(score) {
+    document.getElementById("nom-valid").style.display = "none";
+    document.getElementById("scoreJoueur").style.display = "none";
+    document.getElementById("tousScore").innerHTML = "Voici les scores : " + score;
     console.log(score)
+    document.getElementById("viderTableauScore").style.display = "block";
+                document.getElementById("viderTableauScore").innerHTML = "<p>Voulez vous vider le tableau des scores ? (o/n)</p> <input type='text' id='choixViderScore2' name ='choixViderScore2' placeholder='(O/N)'> <input type='button' id='valider-reset2' value='Valider'>";
+                document.getElementById("valider-reset2").addEventListener("click", function () {
+                let answer2 = document.getElementById("choixViderScore2").value
+                if (answer2 == "o" || answer2 == "O")
+                {
+                    document.getElementById("tousScore").style.display = "none";
+                    reset_score(score)
+                    document.getElementById("viderTableauScore").innerHTML = "<p>Le tableau des scores a été vidé</p>";
+                }})
 }
 
-function reset_score (score)
-{
+function reset_score(score) {
     score = [];
 }
 
-    let checkbox = document.querySelectorAll("input[type=radio]");
+
+
+
+/*function saveScore(score, name) {
+    let tabName = []
+    if(localStorage.getItem('name') != null) {
+    tabName = localStorage.getItem('name');
+    }
+    let tabScore = []
+    if(localStorage.getItem('score') != null){
+        tabScore = localStorage.getItem('score');
+    }
+    
+    for (let i = 0; i < tabScore.length; i++) {
+        tabScore.push(tabScore[i], tabScore[i + 1]);
+    }
+    for (let i = 0; i < tabName.length; i++) {
+        tabName.push(tabName[i], tabName[i + 1]);
+    }
+
+    tabScore.push(score);
+    tabName.push(name);
+
+    console.log(name)
+    localStorage.setItem(score, name);
+    console.log(localStorage.getItem('name'));
+}*/
+
+let affScore = localStorage.getItem('score', 'name');
+function run() {
+    console.log("Vous allez devoir trouver 1 nombre aléatoire entre 1 et 99: \n-----------------")
+    let nb = Math.floor(Math.random() * 99) + 1;
+    console.log(nb)
+    let number
+    let attempt = 0
+    let max_attempt
+
+
+    let checkbox = document.querySelectorAll("input[name=level]");
     for (let check of checkbox) {
         check.addEventListener('change', function () {
             if (this.checked) {
@@ -89,42 +171,17 @@ function reset_score (score)
             document.getElementById("perdu").style.display = "block";
         }
 
-        else{
-            addScore(score, player_name, attempt)
-            console.log("Voulez vous connaitre le score de quelqu'un ? (o/n)\n")
-            let answer = prompt()
-            if (answer == "o" || answer == "O")
-            {
-                getScore(score)
-            }
-
-            console.log("Voulez vous connaitre le tableau des scores ? (o/n)\n")
-            answer = prompt()
-            if (answer == "o" || answer == "O")
-            {
-                getAllScore(score)
-            }
-
-            console.log("Voulez vous videz le tableau des scores ? (o/n)\n")
-            answer = prompt()
-            if (answer == "o" || answer == "O")
-            {
-                score = reset_score(score)
-                console.log("Le tableau des scores a été vidé")
-                console.log(score)
-            }
-
-            /*createFile();
-            positonScore();*/
 
         else if (number > nb) {
             document.getElementById("plus").style.display = "none";
             document.getElementById("moins").style.display = "block";
+            document.getElementById("moins").innerHTML = "<br>nombre d'essais restant: " + (max_attempt - attempt - 1);
 
         }
         else if (number < nb) {
             document.getElementById("moins").style.display = "none";
             document.getElementById("plus").style.display = "block";
+            document.getElementById("plus").innerHTML = "<br>nombre d'essais restant: " + (max_attempt - attempt - 1);
         }
         else {
             console.log("????")
@@ -143,17 +200,78 @@ function reset_score (score)
             document.getElementById("nom-invalid").style.display = "block";
         } else {
             document.getElementById("nom-invalid").style.display = "none";
-            document.getElementById("nom-valid").innerHTML = "<p>Bravo " + player_name + " ! Vous avez gagné en " + attempt + " tentatives !</p>";
-            document.getElementById("nom-valid").style.display = "block";
+            document.getElementById("nom-valid").innerHTML = "<p>Bravo " + player_name + " ! Vous avez gagné en " + attempt + " tentatives !</p>"
             document.getElementById("fin").style.display = "none";
             document.getElementById("gagne").style.display = "none";
+            //saveScore(attempt,player_name)
+            if (score.length ==  max_score_saved*2)
+            {
+                document.getElementById("ScorePlein").style.display = "block";
+                document.getElementById("ScorePlein").innerHTML = "<p>Le tableau des scores est plein, veuillez vider le tableau des scores pour en ajouter de nouveaux<br>Voulez vous vider le tableau des scores ? (o/n)</p> <input type='text' id='choixViderScore' name ='choixViderScore' placeholder='(O/N)'> <input type='button' id='valider-reset' value='Valider'>";
+                document.getElementById("valider-reset").addEventListener("click", function () {
+                let answer2 = document.getElementById("choixViderScore").value
+                if (answer2 == "o" || answer2 == "O")
+                {
+                    reset_score(score)
+                    document.getElementById("ScorePlein").innerHTML = "<p>Le tableau des scores a été vidé</p>";
+                }})
+            }
+            else
+                {
+                document.getElementById("nom-valid").style.display = "block";
+                document.getElementById("score").style.display = "block";
+                addScore(score, player_name, attempt)
+                let checkbox2 = document.querySelectorAll("input[name=choix-oui-non]");
+                let answer
+                for (let check of checkbox2) {
+                    check.addEventListener('change', function () {
+                        if (this.checked) {
+                            console.log(this.value);
+                            answer = ouiNon(this.value)
+                            document.getElementById("score").style.display = "none";
+                            if (answer == true) {
+                                getScore(score)
+
+                            }
+                        }
+                    });
+                }
+        
+
         }
-    })
 
-        /*createFile();
-        positonScore();*/
-   // }
+    }})
 
-}
+        
+    }
+
+
+
+    /* else{
+         
+ 
+         console.log("Voulez vous connaitre le tableau des scores ? (o/n)\n")
+         answer = prompt()
+         if (answer == "o" || answer == "O")
+         {
+             getAllScore(score)
+         }
+ 
+         console.log("Voulez vous videz le tableau des scores ? (o/n)\n")
+         answer = prompt()
+         if (answer == "o" || answer == "O")
+         {
+             score = reset_score(score)
+             console.log("Le tableau des scores a été vidé")
+             console.log(score)
+         }
+ 
+         /*createFile();
+         positonScore();*/
+    /*createFile();
+    positonScore();*/
+    // }*/
+
+
 
 run();
