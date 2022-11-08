d1cd00c33d4914081944a0d05083d4462527f619
Damien <damien@LAPTOP-OE4I5EUT.localdomain>
2022-11-08 20:34:01 +0100

    retour à la normal

--- config.mjs
+++ config.mjs
@@ -1,4 +1,4 @@
-export const max_attempt_facile = 20;
+export const max_attempt_facile = 15;
 export const max_attempt_moyen = 10;
 export const max_attempt_difficile = 5;
 export const max_name_length = 10;
